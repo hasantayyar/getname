f93b8b7d88d4aa3fafc45c5a677cd99bb001a863
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2013-11-18 15:04:56 +0200

    import names

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var mongo = require('mongodb');
 var express = require("express");
 var http = require('http');
 var fs = require('fs');
-
+var counter = 0;
 var app = express();
 app.use(express.logger());
 
@@ -16,27 +16,45 @@ app.get('/importhuman', function(request, response) {
     
     var filename = __dirname+'/humannames.csv';
     console.log('reading '+filename);
-    var readStream = fs.createReadStream(filename);
-    readStream.on('open', function () {
-      readStream.pipe(response);
-    });
-    readStream.on('error', function(err) {
-      response.end(err);
-    });
-
-    //mongo.Db.connect(mongoUri, function (err, db) {
-    //  db.collection('names', function(er, collection) {
-    //    collection.insert({'name': 'myvalue'}, {safe: true}, function(er,rs) {});
-    //  });
-    //});      
-
-    //response.send('imported human names');
-
+    var input = fs.createReadStream(filename);
+    readLines(input, addnew);
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+function readLines(input, addnew) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      addnew(line);
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      addnew(remaining);
+    }
+  });
+}
+
+function addnew(data) {
+  data = data.split(";");
+  console.log(data);
+  mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('names', function(er, collection) {
+      collection.insert({'name': data[1]}, {safe: true}, function(er,rs) {});
+      });
+  });    
+}
+
+
+    
