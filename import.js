df8b00d188b56c1cbbf40d46f3fde59e7c689e8d
hasantayyar <tayyar.besik@gmail.com>
2014-02-24 14:55:11 +0200

    generate pages

---
+++ import.js
@@ -0,0 +1,26 @@
+var mongo = require('mongodb');
+var express = require("express");
+var http = require('http');
+var fs = require('fs');
+var async = require("async")
+     var i =1884;
+     var mnames = require("./humannames.js");
+     var names  = mnames.names().names;
+     var len = names.length;
+     var mongoUri = "mongodb://heroku_app19585045:2a467h22aghopa25bp8e5823ia@ds033499.mongolab.com:33499/heroku_app19585045" || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
+     var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+
+     async.whilst(function () {
+  		return i <= len;
+	},
+	function (next) {
+		console.log(names[i]);
+  		MongoClient.connect(mongoUri, function(err, db) {
+    			if(err) {console.log(err); return;}
+    			db.collection('names').insert(names[i], {w:1}, function(err, objects) { db.close();});
+  		i++;
+                next();
+		});
+		
+     }, function (err){  });
+
--- web.js
+++ web.js
@@ -1,42 +1,83 @@
-var mongo = require('mongodb');
-var express = require("express");
-var http = require('http');
-var fs = require('fs');
-var async = require("async")
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , mongo = require('mongodb');
+var ObjectID = require('mongodb').ObjectID;
+
 var counter = 0;
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('main page!');
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 
-app.get('/importdata', function(request, response) {
-     var i =0;
-     var mnames = require("./humannames.js");
-     var names  = mnames.names();
-     var len = names.length;
-     var mongoUri = "mongodb://tayyar:123450@ds053658.mongolab.com:53658/heroku_app19585045" || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
-     var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
-     async.whilst(function () {
-  		return i <= len;
-	},
-	function (next) {
-  		MongoClient.connect(mongoUri, function(err, db) {
-    			if(err) throw err;
-    			db.collection('names').insert({name: date[1]}, {w:1}, function(err, objects) {});
-  		i++;
-                next();
-		});
-		
-     }, function (err){  });
-
+app.get('/name/:id', function(req,res){
+  mongo.Db.connect(mongoUri, function (err, db) {
+   db.collection('names', function(er, collection) {
+       collection.findOne({"_id":new ObjectID(req.params.id)},function(err,data) {
+            data.title = data.a + " - " +data.t;
+            data.id = data._id;
+           res.render('name', data);
+       });
+     });
+  });
 
 });
+app.get('/rand/:gender',function(req,res){
+  var name = random_name(req.gender,res);
+});
+
+
+var random_name = function(type,res){
+  mongo.Db.connect(mongoUri, function (err, db) {
+    var count = 1;
+    db.collection('names', function(er, collection) {
+      var q = {"type":type};
+      collection.count(q,function(err,data) {
+        if(!data){
+          res.end();
+          return false;
+        }
+        count = data;
+        db.collection('names', function(er, collection) {
+          var rnd = Math.floor(Math.random() * count) + 1;
+          collection.find(q).limit(-1).skip(rnd).next(function(err,data) {
+            if(!data){
+              res.end();
+              return false;
+            }
+            res.writeHead(302, {
+              'Location': '/name/'+data._id
+            });
+            res.end();
+            return false;
+          });
+        });
+      });
+    });
+  });
+}
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
